a78f7543cf53ed54a5ad20eb12b3d0bb39df8fbc
Frederik <fred20057@gmail.com>
2023-09-19 16:10:08 -0400

    update

---
+++ EX9/Personne.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX9
+{
+    public class Personne
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+
+        public Personne(string nom, string prenom, int age = 0)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+        }
+        public string Nom
+        {
+            get { return this.nom; }
+            set { this.nom = value; }
+        }
+        public string Prenom
+        {
+            get { return this.prenom; }
+            set { this.prenom = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public void Afficher()
+        {
+            Console.WriteLine($"Nom: {this.nom} Prenom: {this.prenom} age: {this.age}");
+        }
+    }
+    class Etudiant : Personne
+    {
+        private string domaineEtude;
+        public Etudiant(string nom, string prenom, int age, string domaineEtude) : base(nom, prenom, age)
+        {
+            this.domaineEtude = domaineEtude;
+        }
+        public string DomaineEtude
+        {
+            get { return this.domaineEtude; }
+            set { this.domaineEtude = value; }
+        }
+    }
+}
--- EX9/Program.cs
+++ EX9/Program.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EX5
+﻿namespace EX9
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Personne personne1 = new Personne("test","test",1);
+            Personne personne1 = new Personne("test", "test", 1);
 
             personne1.Afficher();
             personne1.Age = 1;
